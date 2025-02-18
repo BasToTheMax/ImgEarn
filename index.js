7d937366379d62c0c17241cd6d1322e28c03c77e
Bas <55401336+BasToTheMax@users.noreply.github.com>
2025-02-18 11:44:00 +0000

    Empty webserver

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const log = require('logging').default('Main');
+const express = require('express');
+
+const env = process.env;
+const port = env.port || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+    log.info(`App online at port ${port}`);
+});
